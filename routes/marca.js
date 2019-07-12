6158f8caaedc518763757ea1f0d58e5e694c930e
Miguel <magol@hotmail.es>
2019-07-11 20:22:20 -0500

    marca

--- routes/marca.js
+++ routes/marca.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const Marca = require('../models/marca')
 const app = express();
+app.get('/', function(req, res) {
+    res.json('arriba')
+
+
+})
 
 app.get('/app/marca/:id', function(req, res) {
     let id = req.params.id
-    let marca = Marca.findById(id, (error, producto) => {
-        res.json(marca)
+    let marca = Marca.find({ codigo: id }, (error, respuesta) => {
+        res.json(respuesta)
     });
 
 })
 
 app.get('/app/marca', function(req, res) {
     let id = req.params.id
-    let marca = Marca.find({}, (error, producto) => {
-        res.json(marca)
+    let marca = Marca.find({}, (error, respuesta) => {
+        res.json(respuesta)
     });
 
 })
 
 app.post('/app/marca', function(req, res) {
     let body = req.body;
-    let nuevo = new Producto({
+    let nuevo = new Marca({
         codigo: body.codigo,
         nombre: body.nombre
     });

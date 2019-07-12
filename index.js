19481910c6824e5e68c7a2597ff33510db3f006b
Miguel <magol@hotmail.es>
2019-07-11 20:09:33 -0500

    inicio

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+
+
+mongoose.connect('mongodb://localhost:27017/taller', { useNewUrlParser: true }, (error) => {
+    if (error) {
+        throw new Error;
+    }
+    console.log("base conectada");
+});
+
+app.listen(3000, () => {
+    console.log("servidor levantado");
+});
